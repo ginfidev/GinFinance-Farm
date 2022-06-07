a1e09dcbd4272eda50ccfcdfa4b04f779898bbc6
ginfidev <dev@gin.finance>
2022-06-07 00:57:39 -0400

    Fix notify reward amount

--- StakingRewards.sol
+++ StakingRewards.sol
@@ -110,20 +110,24 @@ contract StakingRewards is IStakingRewards, RewardsDistributionRecipient, Reentr
     /* ========== RESTRICTED FUNCTIONS ========== */
 
     function notifyRewardAmount(uint256 reward) external onlyRewardsDistribution updateReward(address(0)) {
+
+        uint256 newDuration = rewardsDuration;
+
         if (block.timestamp >= periodFinish) {
             rewardRate = reward.div(rewardsDuration);
         } else {
             uint256 remaining = periodFinish.sub(block.timestamp);
             uint256 leftover = remaining.mul(rewardRate);
-            rewardRate = reward.add(leftover).div(rewardsDuration.add(remaining));
+            newDuration = newDuration.add(remaining);
+            rewardRate = reward.add(leftover).div(newDuration);
         }
 
         // Ensure the provided reward amount is not more than the balance in the contract.
         // This keeps the reward rate in the right range, preventing overflows due to
         // very high values of rewardRate in the earned and rewardsPerToken functions;
         // Reward + leftover must be less than 2^256 / 10^18 to avoid overflow.
-        uint balance = rewardsToken.balanceOf(address(this));
-        require(rewardRate <= balance.div(rewardsDuration), "Provided reward too high");
+        uint256 balance = rewardsToken.balanceOf(address(this));
+        require(rewardRate <= balance.div(newDuration), "Provided reward too high");
 
         lastUpdateTime = block.timestamp;
         
